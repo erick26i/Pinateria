98f1c7491943f6edc4723552eacc35b41dfc3643
erick <erick261@gmail.com>
2022-08-24 20:49:17 +0200

    debug

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom'
+import { useModal } from './Context/ModalContext'
+import AddComments from './services/AddComments'
+import Header from './components/Header/Header'
+import Home from './components/Home&Register/Home'
+import List from './services/List'
+import Login from './components/Login/Login'
+import Register from './components/Home&Register/Register'
+import CreateService from './services/CreateService'
+import Modal from './components/Modal/Modal'
+import GetUsers from './services/GetUsers'
 
 function App() {
+  const [modal] = useModal();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/service/list' element={<List />} />
+        <Route path='/service/user/task' element={<AddComments />} />
+        <Route path='/service/add' element={<CreateService />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/service/users' element={<GetUsers />} />
+      </Routes>
+
+      {modal && <Modal>{modal}</Modal>}
     </div>
-  );
+  )
 }
 
 export default App;
